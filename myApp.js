8ee005097e59e412c15fad3627b5ffcc3b483f05
Erschindel <erschindel@gmail.com>
2019-12-08 00:08:47 -0500

    completed basic routing

--- myApp.js
+++ myApp.js
@@ -1,52 +1,77 @@
-
-var express = require('express');
+var express = require("express");
 var app = express();
+let bodyParser = require("body-parser");
 
 // --> 7)  Mount the Logger middleware here
-
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
 // --> 11)  Mount the body-parser middleware  here
-
+app.use(bodyParser.urlencoded({extended: false}))
 
 /** 1) Meet the node console. */
-
+console.log("Hello World");
 
 /** 2) A first working Express Server */
-
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// })
 
 /** 3) Serve an HTML file */
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
 
-
 /** 6) Use the .env file to configure the app */
- 
- 
+process.env.MESSAGE_STYLE = "uppercase";
+
+app.get("/json", (req, res) => {
+  process.env.MESSAGE_STYLE === "uppercase"
+    ? res.json({ message: "HELLO JSON" })
+    : res.json({ message: "Hello json" });
+});
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
-
 /** 8) Chaining middleware. A Time server */
-
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo", (req, res) => {
+  res.json({ echo: req.params.word });
+});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
+app.get("/name?:first?:last", (req, res) => {
+  res.json({ name: req.query.first + " " + req.query.last});
+});
 
-  
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
-
 /** 12) Get data form POST  */
 
-
+app.post("/name?:first?:last", (req, res) => {
+  res.json({name: req.body.first + " " + req.body.last})
+})
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
@@ -54,4 +79,4 @@ var app = express();
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
- module.exports = app;
+module.exports = app;
